5462961c700ea5aa9277fea298e284c83f2e1c47
nikkin <nicol.nilsen@gmail.com>
2024-11-15 20:15:30 -0500

    a

---
+++ src/app/artist-items/artist-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './artist-items.component.html',
+  styleUrl: './artist-items.component.css'
+})
+export class ArtistItemsComponent {
+
+}
